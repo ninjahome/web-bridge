724932503fd78e86c58404d0c1171576abb9138d
basdevelop <hyperorchid@outlook.com>
2023-12-17 17:07:16 +0800

    test of server

--- server/test/firestore.go
+++ server/test/firestore.go
@@ -11,15 +11,13 @@ import (
 func main() {
 	ctx := context.Background()
 
-	// 使用服务账户密钥文件创建Firestore客户端
 	client, err := firestore.NewClient(ctx, "dessage", option.WithCredentialsFile("dessage-c3b5c95267fb.json"))
 	if err != nil {
 		log.Fatalf("Failed to create Firestore client: %v", err)
 	}
 	defer client.Close()
 
-	// 示例：添加文档
-	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+	_, _, err = client.Collection("dessage-twitter").Add(ctx, map[string]interface{}{
 		"first": "Ada",
 		"last":  "Lovelace",
 		"born":  1815,
