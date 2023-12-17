e66085861e277456407e3a3db76584d0c76e83bb
basdevelop <hyperorchid@outlook.com>
2023-12-17 17:12:22 +0800

    test of server

--- server/test/firestore.go
+++ server/test/firestore.go
@@ -11,16 +11,16 @@ import (
 func main() {
 	ctx := context.Background()
 
-	client, err := firestore.NewClient(ctx, "dessage", option.WithCredentialsFile("dessage-c3b5c95267fb.json"))
+	client, err := firestore.NewClient(ctx, "dessage-twitter", option.WithCredentialsFile("dessage-c3b5c95267fb.json"))
 	if err != nil {
 		log.Fatalf("Failed to create Firestore client: %v", err)
 	}
 	defer client.Close()
 
-	_, _, err = client.Collection("dessage-twitter").Add(ctx, map[string]interface{}{
-		"first": "Ada",
-		"last":  "Lovelace",
-		"born":  1815,
+	_, _, err = client.Collection("twitter-user").Add(ctx, map[string]interface{}{
+		"id":       "Ada",
+		"name":     "Lovelace",
+		"username": "1815",
 	})
 	if err != nil {
 		log.Fatalf("Failed to add a new user: %v", err)
