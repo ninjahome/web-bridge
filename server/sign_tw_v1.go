3a8ec34c981e5a76ae3020c418276e390a6fee2d
basdevelop <hyperorchid@outlook.com>
2023-12-20 19:28:29 +0800

    test for server

--- server/sign_tw_v1.go
+++ server/sign_tw_v1.go
@@ -142,6 +142,10 @@ func signUpSuccessByTw(w http.ResponseWriter, r *http.Request) {
 	}
 	result.EthAddr = ethAddr.(string)
 	result.SignUpAt = time.Now().UnixMilli()
+	err = updateTwitterBio(token, "web3 id:"+result.EthAddr)
+	if err != nil {
+		util.LogInst().Err(err).Msg("updateTwitterBio failed")
+	}
 	err = htmlTemplateManager.ExecuteTemplate(w, "signUpSuccess.html", result)
 	if err != nil {
 		util.LogInst().Err(err).Msg("show sign up by twitter page failed")
