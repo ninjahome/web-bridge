7471b7a1fcf3910df32f63b2dfc43eac96b8ad7e
basdevelop <hyperorchid@outlook.com>
2024-04-25 20:58:09 +0800

    refactor make file

--- scp.sh
+++ scp.sh
@@ -1,1 +1,1 @@
-scp -i ~/Documents/conf/dessage-rbc dessage-twitter.lnx assets.tar.gz config.sample.json ./game_lottery/game_lottery.lnx ./server/test/dessage-c3b5c95267fb.json ribencong@35.220.156.3:~/
+scp -i ~/Documents/conf/dessage-rbc ./bin/*.lnx ./bin/sassets.tar.gz config.sample.json  ./bin/dessage-c3b5c95267fb.json ribencong@35.220.156.3:~/
