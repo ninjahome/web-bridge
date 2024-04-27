f622493344b9fa61e28d1865daacb5864d127d96
basdevelop <hyperorchid@outlook.com>
2024-04-27 12:26:51 +0800

    a test tool

--- server/test/firestore.go
+++ server/test/firestore.go
@@ -3,11 +3,15 @@ package main
 import (
 	"cloud.google.com/go/firestore"
 	"context"
+	"google.golang.org/api/option"
 	"log"
 	"os"
 )
 
 func main() {
+	UpdateField()
+}
+func test() {
 	ctx := context.Background()
 	os.Setenv("FIRESTORE_EMULATOR_HOST", "localhost:8080")
 	//client, err := firestore.NewClient(ctx, "dessage", option.WithCredentialsFile("dessage-c3b5c95267fb.json"))
@@ -25,4 +29,27 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to add a new user: %v", err)
 	}
+}
+
+func UpdateField() {
+	ctx := context.Background()
+	saPath := "dessage-c3b5c95267fb.json"
+	client, err := firestore.NewClientWithDatabase(ctx, "dessage",
+		"dessage-release", option.WithCredentialsFile(saPath))
+	if err != nil {
+		log.Fatalf("Failed to create Firestore client: %v", err)
+	}
+	defer client.Close()
+
+	// 假设有一个文档 "document-id" 在集合 "your-collection" 中
+	docRef := client.Collection("tweets-posted").Doc("1714188650865")
+
+	// 更新字段，添加回车换行
+	newContent := "Hello World, now we are coming!\nLink the world, break down barriers, individualism means everything!" // 这里 "\n" 是换行符
+	_, err = docRef.Update(ctx, []firestore.Update{{Path: "text", Value: newContent}})
+	if err != nil {
+		log.Fatalf("Failed to update document: %v", err)
+	}
+
+	log.Println("Document updated successfully with newline")
 }
