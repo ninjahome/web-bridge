e1ba7b9c852e58df6af79c100ccd3724c8c57f44
basdevelop <hyperorchid@outlook.com>
2024-04-27 12:06:03 +0800

    bug for txt show

--- assets/js/main_common.js
+++ assets/js/main_common.js
@@ -256,8 +256,8 @@ async function setupCommonTweetHeader(tweetHeader, tweet, overlap) {
     tweetHeader.querySelector('.tweetCreateTime').textContent = formatTime(tweet.create_time);
     const twitterObj = await __setOnlyHeader(tweetHeader, tweet.twitter_id, tweet.web3_id);
     const contentArea = tweetHeader.querySelector('.tweet-content');
-    // console.log(tweet.text);
-    contentArea.innerText = tweet.text;//DOMPurify.sanitize(tweet.text.replace(/\n/g, "<br>"));
+    console.log(tweet.text);
+    contentArea.innerHTML = DOMPurify.sanitize(tweet.text.replace(/\n/g, "<br>"));
 
     const wrappedHeader = tweetHeader.querySelector('.tweet-header');
 
